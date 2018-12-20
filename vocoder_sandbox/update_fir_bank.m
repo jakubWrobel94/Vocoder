120282303ae050cf9e137aaf147a6a225a1a882c
Kuba <jakub.wrobel94@gmail.com>
2018-12-20 10:32:08 +0100

    improved performance when updating filter bank

--- vocoder_sandbox/update_fir_bank.m
+++ vocoder_sandbox/update_fir_bank.m
@@ -1,6 +1,4 @@
 function [fir_bank_out] = update_fir_bank(fir_bank, filt_coef)
-fir_bank_out = fir_bank;
-for n = 1:size(fir_bank,1)
-    fir_bank_out(n, :) = filt_coef(n)* fir_bank(n, :);
-end
+filt_coef_matrix = repmat(filt_coef', 1, size(fir_bank,2)); 
+fir_bank_out = fir_bank .* filt_coef_matrix;
 end
